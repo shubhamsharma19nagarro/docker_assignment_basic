e810e66822151893d33b0ac324bc1e55b2a0685b
shubhamsharma19nagarro <shubham.sharma19@nagarro.com>
2025-01-16 17:54:23 +0530

    employeeController changes commiting

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -16,14 +16,14 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-//    Rest Api to get all Employees
+//    Rest API to get all Employees
     @GetMapping("/get")
     public ResponseEntity<List<Employee>> getAllEmployee() {
         List<Employee> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
     }
 
-//  Rest Api to create Employee
+//  Rest API to create or save Employee
     @PostMapping("/add")
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         Employee saveEmployee = employeeService.saveEmployee(employee);
